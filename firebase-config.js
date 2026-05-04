59106fb2e3e7514f2ac559a996803c96c963f666
nickhebocker <nickhebocker@gmail.com>
2026-05-04 10:32:17 -0500

    Add Firebase configuration template file

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase configuration template
+
+const firebaseConfig = {
+    apiKey: '', // Your API key
+    authDomain: '', // Your Auth domain
+    projectId: '', // Your Project ID
+    storageBucket: '', // Your Storage Bucket
+    messagingSenderId: '', // Your Messaging Sender ID
+    appId: '' // Your App ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
